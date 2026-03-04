da492aa12e57c4ec136dcb936e9c2478a23daa8f
Nikita Litvinenko <nikitalitvinenko28@gmail.com>
2026-03-04 15:36:41 +0200

    Update RGBPage.xaml.cs

--- Example/RGBPage.xaml.cs
+++ Example/RGBPage.xaml.cs
@@ -1,4 +1,6 @@
+using Microsoft.Maui.Controls;
 using Microsoft.Maui.Layouts;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Example;
 
@@ -8,34 +10,37 @@ public partial class RGBPage : ContentPage
     private Label _redLabel, _greenLabel, _blueLabel;
     private BoxView _boxView;
     private BoxView _redBox, _greenBox, _blueBox;
-    private Stepper _sizeStepper;
+    private Stepper _rgbStepper;
+    private Label _title;
 
     public RGBPage()
     {
         var layout = new AbsoluteLayout();
-        var title = new Label
+        _title = new Label
         {
-            Text = "RGB",
+            Text = "RGB mudel",
             FontSize = 26,
             TextColor = Colors.SeaGreen
         };
 
-        AbsoluteLayout.SetLayoutBounds(title, new Rect(0.5, 0.05, -1, -1));
-        AbsoluteLayout.SetLayoutFlags(title, AbsoluteLayoutFlags.PositionProportional);
+        AbsoluteLayout.SetLayoutBounds(_title, new Rect(0.5, 0.05, -1, -1));
+        AbsoluteLayout.SetLayoutFlags(_title, AbsoluteLayoutFlags.PositionProportional);
 
         // ----------------------------------------------
-        _redBox = CreateColorBox(0.25);
-        _greenBox = CreateColorBox(0.5);
-        _blueBox = CreateColorBox(0.75);
+        double offsetY = 0.2;
 
-        _redSlider = CreateSlider(0.20);
-        _redLabel = CreateLabel("Punane = 00", 0.25);
+        _redBox = CreateColorBox(0.2, 0.2 + offsetY);
+        _greenBox = CreateColorBox(0.2, 0.3 + offsetY);
+        _blueBox = CreateColorBox(0.2, 0.4 + offsetY);
 
-        _greenSlider = CreateSlider(0.30);
-        _greenLabel = CreateLabel("Roheline = 00", 0.35);
+        _redSlider = CreateSlider(0.2 + offsetY);
+        _redLabel = CreateLabel("Punane = 00", 0.225 + offsetY);
 
-        _blueSlider = CreateSlider(0.40);
-        _blueLabel = CreateLabel("Sinine = 00", 0.45);
+        _greenSlider = CreateSlider(0.3 + offsetY);
+        _greenLabel = CreateLabel("Roheline = 00", 0.325 + offsetY);
+
+        _blueSlider = CreateSlider(0.4 + offsetY);
+        _blueLabel = CreateLabel("Sinine = 00", 0.425 + offsetY);
     
         // ----------------------------------------------
         var rndBut = new Button { Text = "Juhuslik värv" };
@@ -45,33 +50,66 @@ public partial class RGBPage : ContentPage
             _greenSlider.Value = Random.Shared.Next(0, 256);
             _blueSlider.Value = Random.Shared.Next(0, 256);
         };
-        AbsoluteLayout.SetLayoutBounds(rndBut, new Rect(0.5, 0.65, 200, 50));
+        AbsoluteLayout.SetLayoutBounds(rndBut, new Rect(0.2, 0.525 + offsetY, 150, 50));
         AbsoluteLayout.SetLayoutFlags(rndBut, AbsoluteLayoutFlags.PositionProportional);
 
         // ----------------------------------------------
         _boxView = new BoxView
         {
-            Color = Colors.Black
+            Color = Colors.Black,
+            CornerRadius = 15,
         };
-        AbsoluteLayout.SetLayoutBounds(_boxView, new Rect(0.5, 0.95, 180, 180));
+        AbsoluteLayout.SetLayoutBounds(_boxView, new Rect(0.5, 0.15, 180, 180));
         AbsoluteLayout.SetLayoutFlags(_boxView, AbsoluteLayoutFlags.PositionProportional);
 
-        _sizeStepper = new Stepper
+        // ----------------------------------------------
+        _rgbStepper = new Stepper
         {
-            Minimum = 100,
-            Maximum = 250,
+            Minimum = double.MinValue,
+            Maximum = double.MaxValue,
             Increment = 10,
-            Value = 180
+            Value = 0,
         };
-        _sizeStepper.ValueChanged += (sender, e) => 
+
+        AbsoluteLayout.SetLayoutBounds(_rgbStepper, new Rect(0.6, 0.525 + offsetY, -1, -1));
+        AbsoluteLayout.SetLayoutFlags(_rgbStepper, AbsoluteLayoutFlags.PositionProportional);
+
+        _rgbStepper.ValueChanged += (s, e) =>
         {
-            AbsoluteLayout.SetLayoutBounds(_boxView, new Rect(0.5, 1.2, e.NewValue, e.NewValue));
+            double delta = e.NewValue - e.OldValue;
+            _redSlider.Value = Math.Clamp(_redSlider.Value + delta, 0, 255);
+            _greenSlider.Value = Math.Clamp(_greenSlider.Value + delta, 0, 255);
+            _blueSlider.Value = Math.Clamp(_blueSlider.Value + delta, 0, 255);
         };
 
-        AbsoluteLayout.SetLayoutBounds(_sizeStepper, new Rect(0.5, 1.4, -1, -1));
-        AbsoluteLayout.SetLayoutFlags(_sizeStepper, AbsoluteLayoutFlags.PositionProportional);
+        // ----------------------------------------------
+        var stepLabel = new Label { Text = _rgbStepper.Increment.ToString() };
+        AbsoluteLayout.SetLayoutBounds(stepLabel, new Rect(0.5, 0.625 + offsetY, -1, -1));
+        AbsoluteLayout.SetLayoutFlags(stepLabel, AbsoluteLayoutFlags.PositionProportional);
 
-        layout.Children.Add(title);
+        var stepSlider = new Slider
+        {
+            Value = _rgbStepper.Increment,
+            Minimum = 0,
+            Maximum = 100,
+        };
+        stepSlider.ValueChanged += (sender, e) => {
+            double step = 1;
+            var newStep = Math.Round(e.NewValue / step) * step;
+            ((Slider)sender).Value = newStep;
+
+            _rgbStepper.Increment = stepSlider.Value;
+            stepLabel.Text = stepSlider.Value.ToString();
+        };
+        AbsoluteLayout.SetLayoutBounds(stepSlider, new Rect(0.8, 0.6 + offsetY, 200, 40));
+        AbsoluteLayout.SetLayoutFlags(stepSlider, AbsoluteLayoutFlags.PositionProportional);
+
+        _redSlider.Value = _rgbStepper.Value;
+        _greenSlider.Value = _rgbStepper.Value;
+        _blueSlider.Value = _rgbStepper.Value;
+
+        // ----------------------------------------------
+        layout.Children.Add(_title);
 
         layout.Children.Add(_redBox);
         layout.Children.Add(_greenBox);
@@ -87,20 +125,23 @@ public partial class RGBPage : ContentPage
         layout.Children.Add(rndBut);
 
         layout.Children.Add(_boxView);
-        layout.Children.Add(_sizeStepper);
+        layout.Children.Add(_rgbStepper);
+        layout.Children.Add(stepLabel);
+        layout.Children.Add(stepSlider);
 
         Content = layout;
     }
 
-    private BoxView CreateColorBox(double x)
+    private BoxView CreateColorBox(double x, double y)
     {
         var box = new BoxView
         {
             WidthRequest = 65,
             HeightRequest = 60,
-            CornerRadius = 15
+            CornerRadius = 15,
+            Color = Colors.Black,
         };
-        AbsoluteLayout.SetLayoutBounds(box, new Rect(x, 0.12, 65, 60));
+        AbsoluteLayout.SetLayoutBounds(box, new Rect(x, y, 65, 60));
         AbsoluteLayout.SetLayoutFlags(box, AbsoluteLayoutFlags.PositionProportional);
         return box;
     }
@@ -113,7 +154,7 @@ public partial class RGBPage : ContentPage
             Maximum = 255
         };
         s.ValueChanged += OnSliderValueChanged;
-        AbsoluteLayout.SetLayoutBounds(s, new Rect(0.5, y, 300, 40));
+        AbsoluteLayout.SetLayoutBounds(s, new Rect(0.65, y, 220, 40));
         AbsoluteLayout.SetLayoutFlags(s, AbsoluteLayoutFlags.PositionProportional);
         return s;
     }
@@ -121,7 +162,7 @@ public partial class RGBPage : ContentPage
     private Label CreateLabel(string text, double y)
     {
         var l = new Label { Text = text };
-        AbsoluteLayout.SetLayoutBounds(l, new Rect(0.5, y, -1, -1));
+        AbsoluteLayout.SetLayoutBounds(l, new Rect(0.45, y, -1, -1));
         AbsoluteLayout.SetLayoutFlags(l, AbsoluteLayoutFlags.PositionProportional);
         return l;
     }
@@ -135,10 +176,17 @@ public partial class RGBPage : ContentPage
         else if (sender == _blueSlider)
             _blueLabel.Text = String.Format("Sinine = {0:X2}", (int)args.NewValue);
 
-        _boxView.Color = Color.FromRgb((int)_redSlider.Value, (int)_greenSlider.Value, (int)_blueSlider.Value);
+        _boxView.Color = Color.FromRgb(((int)_redSlider.Value) / 255.0, ((int)_greenSlider.Value) / 255.0, ((int)_blueSlider.Value) / 255.0);
+        _title.TextColor = Color.FromRgb(((int)_redSlider.Value) / 255.0, ((int)_greenSlider.Value) / 255.0, ((int)_blueSlider.Value) / 255.0);
 
-        _redBox.Color = Color.FromRgb((int)_redSlider.Value, 0, 0);
-        _greenBox.Color = Color.FromRgb(0, (int)_greenSlider.Value, 0);
-        _blueBox.Color = Color.FromRgb(0, 0, (int)_blueSlider.Value);
+        _redBox.Color = Color.FromRgb(((int)_redSlider.Value) / 255.0, 0, 0);
+        _greenBox.Color = Color.FromRgb(0, ((int)_greenSlider.Value) / 255.0, 0);
+        _blueBox.Color = Color.FromRgb(0, 0, ((int)_blueSlider.Value) / 255.0);
+
+        Dispatcher.Dispatch(async () =>
+        {
+            await _boxView.ScaleToAsync(1.1, 100);
+            await _boxView.ScaleToAsync(1.0, 100);
+        });
     }
 }
